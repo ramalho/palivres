9cf31d502797c20c051e8cb385d8e8e454f1e082
Luciano Ramalho <luciano@ramalho.org>
2016-11-19 00:32:45 -0200

    Vocabulário Ortográfico 2016

--- stats.py
+++ stats.py
@@ -2,13 +2,14 @@
 
 import collections
 import sys
+import string
 
 contador = collections.Counter()
 
 with open(sys.argv[1], 'rt', encoding='utf-8') as fp:
     for lin in fp:
         car = lin[0].lower()
-        if car.isalpha():
+        if car in string.ascii_lowercase:  # car.isalpha():
             contador[car] += 1
 
 for car, n in contador.most_common():
